8dad67d8d13f55c91b33fd55599593ebea555c00
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2022-04-02 16:10:40 +0200

    Mock UserRepository instead of using fake

--- src/test/java/org/example/ood/RegisterUserTest.java
+++ src/test/java/org/example/ood/RegisterUserTest.java
@@ -15,43 +15,58 @@ public class RegisterUserTest {
     final JUnit5Mockery context = new JUnit5Mockery();
 
     private final Emailer emailer = context.mock(Emailer.class);
-    private final UserRepository users = new InMemoryUserRepository();
+    private final UserRepository users = context.mock(UserRepository.class);
     private final RegisterUser registerUser = new RegisterUser(users, emailer);
 
     @Test
     void givenValidUsernameAndPasswordThenTheUserIsRegisteredAndItSendsTheUserAWelcomeEmail() {
         context.checking(new Expectations() {{
+            allowing(users).exists("username"); will(returnValue(false));
+            oneOf(users).save(new User("username", "securepassword", "user@example.com"));
             oneOf(emailer).send("user@example.com", "us@example.org", "Welcome, username! Let me explain at length how to get started using this service! ...");
         }});
         assertEquals(true, registerUser.execute("username", "securepassword", "user@example.com"));
-        assertEquals(true, users.exists("username"));
     }
 
     @Test
     void givenEmptyUsernameThenRegistrationFails() {
+        context.checking(new Expectations() {{
+            allowing(users).exists("username"); will(returnValue(false));
+        }});
         assertEquals(false, registerUser.execute("", "securepassword", "user@example.com"));
         assertEquals(false, users.exists("username"));
     }
 
     @Test
     void givenEmptyPasswordThenRegistrationFails() {
+        context.checking(new Expectations() {{
+            allowing(users).exists("username"); will(returnValue(false));
+        }});
         assertEquals(false, registerUser.execute("username", "", "user@example.com"));
         assertEquals(false, users.exists("username"));
     }
 
     @Test
     void givenTooShortPasswordThenRegistrationFails() {
+        context.checking(new Expectations() {{
+            allowing(users).exists("username"); will(returnValue(false));
+        }});
         assertEquals(false, registerUser.execute("username", "short", "user@example.com"));
     }
 
     @Test
     void givenEmptyEmailThenRegistrationFails() {
+        context.checking(new Expectations() {{
+            allowing(users).exists("username"); will(returnValue(false));
+        }});
         assertEquals(false, registerUser.execute("username", "securepassword", ""));
     }
 
     @Test
     void givenUserWithSameUsernameExistsThenRegistrationFails() {
-        users.save(new User("existinguser", "irrelevantPassword", "irrelevant@example.com"));
+        context.checking(new Expectations() {{
+            allowing(users).exists("existinguser"); will(returnValue(true));
+        }});
         assertEquals(false, registerUser.execute("existinguser", "securepassword", "user@example.com"));
     }
 
