dda5ad36ebf43ed14ef221f4778edb87e5ebd069
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2022-04-02 15:12:40 +0200

    Save user in repository on successful registration

--- src/test/java/org/example/ood/RegisterUserTest.java
+++ src/test/java/org/example/ood/RegisterUserTest.java
@@ -2,14 +2,20 @@ package org.example.ood;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RegisterUserTest {
-    private final RegisterUser registerUser = new RegisterUser();
+    private final UserRepository users = new UserRepository();
+    private final RegisterUser registerUser = new RegisterUser(users);
 
     @Test
     void givenValidUsernameAndPasswordThenTheUserIsRegistered() {
         assertEquals(true, registerUser.execute("username", "password"));
+        assertEquals(true, users.exists("username"));
     }
 
     @Test
@@ -23,11 +29,30 @@ public class RegisterUserTest {
     }
 
     private static class RegisterUser {
+        private final UserRepository users;
+
+        public RegisterUser(UserRepository users) {
+            this.users = users;
+        }
+
         public boolean execute(String username, String password) {
             if ("".equals(username) || "".equals(password)) {
                 return false;
             }
+            users.save(username);
             return true;
+        }
+    }
+
+    private static class UserRepository {
+        private final List<String> users = new ArrayList<>();
+
+        boolean exists(String username) {
+            return users.contains(username);
+        }
+
+        void save(String username) {
+            users.add(username);
         }
     }
 }
