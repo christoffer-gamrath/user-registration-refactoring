01e81ba662c63b97225122a1b994c7b668c2dc16
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2022-04-02 12:37:08 +0200

    Scaffold test case

---
+++ src/test/java/org/example/ood/RegisterUserTest.java
@@ -0,0 +1,12 @@
+package org.example.ood;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RegisterUserTest {
+    @Test
+    void registerUser() {
+        assertEquals(true, true);
+    }
+}
