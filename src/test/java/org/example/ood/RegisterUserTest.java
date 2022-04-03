a8e61df5180da0e56ed61ad510b4c23bdb5dd799
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2022-04-03 08:03:18 +0200

    Optimize imports

--- src/test/java/org/example/ood/RegisterUserTest.java
+++ src/test/java/org/example/ood/RegisterUserTest.java
@@ -5,9 +5,6 @@ import org.jmock.junit5.JUnit5Mockery;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RegisterUserTest {
